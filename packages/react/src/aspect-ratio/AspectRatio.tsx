f267fa70ffc2bb1070c7b896ffa18fa81a86a14c
Jacksonmills <jacksonemills@gmail.com>
2025-06-29 07:44:33 -0500

    refactor(aspect-ratio): simplify implementation using native CSS aspect-ratio
    Replaced the old padding-bottom hack and extra wrapper div with the native aspect-ratio CSS property.

--- packages/react/src/aspect-ratio/AspectRatio.tsx
+++ packages/react/src/aspect-ratio/AspectRatio.tsx
@@ -28,29 +28,13 @@ export const AspectRatio = React.forwardRef(function AspectRatio(
       style: {
         ...style,
         // ensures children expand in ratio
-        position: 'absolute',
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
+        aspectRatio: ratio,
       },
       children,
     },
   });
 
-  return (
-    <div
-      style={{
-        // ensures inner element is contained
-        position: 'relative',
-        // ensures padding bottom trick maths works
-        width: '100%',
-        paddingBottom: `${100 / ratio}%`,
-      }}
-    >
-      {element}
-    </div>
-  );
+  return element
 });
 
 export namespace AspectRatio {
